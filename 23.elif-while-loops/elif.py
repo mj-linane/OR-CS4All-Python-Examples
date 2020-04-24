f9c5c5ad84ccf8eaf3e5608e5bacfa4a1abdd127
mj-linane <mj@mjlinane.com>
2020-04-24 15:33:57 -0400

    Add new line escape

--- 23.elif-while-loops/elif.py
+++ 23.elif-while-loops/elif.py
@@ -1,6 +1,6 @@
 # ELIF STATEMENTS
 
-favorite = input("What is your favorite flavor of ice cream ")
+favorite = input("\nWhat is your favorite flavor of ice cream?\n")
 
 if favorite == "vanilla":
     print("your favorite flavor is vanilla")
