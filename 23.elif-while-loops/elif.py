790dc30efdb12e27e4cfd41ae40026e57acb5687
mj-linane <19750620+mj-linane@users.noreply.github.com>
2020-04-24 14:53:35 -0400

    Initial

---
+++ 23.elif-while-loops/elif.py
@@ -0,0 +1,21 @@
+# ELIF STATEMENTS
+
+favorite = input("What is your favorite flavor of ice cream")
+
+if favorite == "vanilla":
+    print("your favorite flavor is vanilla")
+
+elif favorite == "chocolate":
+    print("your favorite flavor is chocolate")
+
+elif favorite == "strawberry":
+    print("your favorite flavor is strawberry")
+
+elif favorite == "coffee":
+    print("your favorite flavor is coffee")
+
+elif favorite == "mint":
+    print("your favorite flavor is mint")
+
+else:
+    print("you don't like ice cream")
