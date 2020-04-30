fca7f81565602d94f3cd0aea6e5385eb58e94cf4
mj-linane <mj@mjlinane.com>
2020-04-30 17:18:53 -0400

    Delete unused

--- PythonTTS.py
+++
@@ -1,4 +0,0 @@
-import win32com.client as wincl
-
-speak = wincl.Dispatch("SAPI.SpVoice")
-speak.Speak('Hello Scott, I represent all the robots at Old Rochester High School. Please help us Scott. You are our only hope')
