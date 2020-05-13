f588f834bea809b1e5dcfb3b5efb9241bc76fcfe
mj-linane <mj@mjlinane.com>
2020-05-13 10:41:41 -0400

    Add additional examples

--- 30.function-return-birthday-countdown-tracker/return-functions.py
+++ 30.function-return-birthday-countdown-tracker/return-functions.py
@@ -12,3 +12,22 @@ def add(x, y):
 
 add_answer = add(1, 2)
 print(add_answer)
+
+# Shortened form
+
+
+def addShort(x, y):
+    return x + y
+
+
+print(addShort(2, 3))
+
+
+def addMulti(x, y):
+    return (
+        x +
+        y
+    )
+
+
+print(addMulti(2, 3))
