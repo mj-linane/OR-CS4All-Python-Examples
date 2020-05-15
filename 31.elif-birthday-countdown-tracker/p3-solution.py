19a4f11fbe008e623b296e58c07df6ef80813975
mj-linane <mj@mjlinane.com>
2020-05-15 11:13:18 -0400

    cleaned up code

--- 31.elif-birthday-countdown-tracker/p3-solution.py
+++ 31.elif-birthday-countdown-tracker/p3-solution.py
@@ -14,7 +14,7 @@ people = {
 }
 
 
-def get_days_until_bday(person):
+def get_days_until_bday(name):
     # Get today
     now = datetime.datetime.today()
     yr = now.year
@@ -22,7 +22,7 @@ def get_days_until_bday(person):
     day = now.day
 
     # Get Birthday and Convert to Datetime
-    birthday = datetime.date(yr, person["month"], person["day"])
+    birthday = datetime.date(yr, people[name]["month"], people[name]["day"])
 
     # Format today's date for compile
     today_date = datetime.date(yr, month, day)
@@ -34,28 +34,28 @@ def get_days_until_bday(person):
 
 def report_person_days_left(name):
     # Here we can assign a variable to the returned value of a function
-    num_days = get_days_until_bday(people[name])
+    num_days = get_days_until_bday(name)
 
     # Check for difference
     if num_days == 0:
         return (
-            name.capitalize()
-            + "'s birthday is today!"
+            name.capitalize() +
+            "'s birthday is today!"
         )
     elif num_days < 0:
         num_days = num_days + 365
         return (
-            name.capitalize()
-            + "'s birthday is in "
-            + str(num_days)
-            + " days."
+            name.capitalize() +
+            "'s birthday is in " +
+            str(num_days) +
+            " days." +
         )
     else:
         return (
-            name.capitalize()
-            + "'s birthday is in "
-            + str(num_days)
-            + " days."
+            name.capitalize() +
+            "'s birthday is in " +
+            str(num_days) +
+            " days."
         )
 
 
