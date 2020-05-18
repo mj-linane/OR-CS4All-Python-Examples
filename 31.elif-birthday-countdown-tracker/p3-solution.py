427aa15a6f8c21da8e3f4dd73287c6834e09e1c1
mj-linane <mj@mjlinane.com>
2020-05-18 12:43:50 -0400

    Matched code to student example

--- 31.elif-birthday-countdown-tracker/p3-solution.py
+++ 31.elif-birthday-countdown-tracker/p3-solution.py
@@ -1,16 +1,27 @@
 # Birthday Countdown Tracker
+import datetime
 """
 - Keep a list of people and their birthdays
 - Calculate days until their birthdays
 - Report to me a list of birthdays
 """
 
-import datetime
-
 people = {
-    "JONAH": {"year": 2000, "month": 12, "day": 19},
-    "TESS": {"year": 2003, "month": 5, "day": 5},
-    "ADAM": {"year": 2003, "month": 5, "day": 8},
+    "JONAH": {
+        "year": 2000,
+        "month": 12,
+        "day": 19
+    },
+    "TESS": {
+        "year": 2003,
+        "month": 5,
+        "day": 15
+    },
+    "ADAM": {
+        "year": 2000,
+        "month": 5,
+        "day": 7
+    },
 }
 
 
@@ -32,16 +43,17 @@ def get_days_until_bday(name):
     return int(difference.days)
 
 
-def report_person_days_left(name):
-    # Here we can assign a variable to the returned value of a function
+def report_days_left(name):
+    # Assign a variable to the returned value of the function
     num_days = get_days_until_bday(name)
 
     # Check for difference
-    if num_days == 0:
+    if num_days == 0:  # If birthday is today
         return (
             name.capitalize() +
             "'s birthday is today!"
         )
+    # Past birthdays
     elif num_days < 0:
         num_days = num_days + 365
         return (
@@ -50,6 +62,8 @@ def report_person_days_left(name):
             str(num_days) +
             " days."
         )
+
+    # Future birthdays
     else:
         return (
             name.capitalize() +
@@ -61,7 +75,7 @@ def report_person_days_left(name):
 
 def report_all_birthdays():
     for name in people:
-        print(report_person_days_left(name))
+        print(report_days_left(name))
 
 
 report_all_birthdays()
